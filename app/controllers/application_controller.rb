2a9d2f8077f676e8f5d2d9dc2733394e3a7cc19e
kns3-3 <toma66kfc@docomo.ne.jp>
2026-04-03 18:42:33 +0900

    新規ユーザー登録画面の作成(途中)とヘッダーにログイン・ログアウトの条件分岐を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  before_action :configure_permitted_parameters, if: :devise_controller? #アクション前に独自メソッドを実行
+
+  protected #ApplicationControllerを継承した子クラス(Deviseなど)からのみアクセス可能
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name]) #sign_upの際にnameカラムの保存を許可
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :introduction]) #account_updateの際にname,introductionカラムの保存を許可
+  end
 end
