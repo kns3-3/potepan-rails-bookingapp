70bb8d4e454d91703b725b04936739d6ae620420
kns3-3 <toma66kfc@docomo.ne.jp>
2026-04-08 17:20:13 +0900

    施設検索機能と国際化の実装
    - 検索画面のレイアウト作成とエリア・キーワード検索の実装
    - 施設登録・編集のバリデーション修正と必須マークの追加
    - rails-i18nによるアプリの日本語化（Devise含む）

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,5 +3,7 @@ class Room < ApplicationRecord
   has_one_attached :image #施設用の写真を1枚持つ
 
   validates :name, :description, :price, :address, presence: true
-  validates :price, numericality: { greater_than_or_equal_to: 1 } #金額の登録は1円以上
+
+  #宿泊料金は、数値、整数、1以上を条件とする
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
 end
