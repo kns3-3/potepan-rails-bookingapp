39bed456c07de00e71bcd25427dc5c9480422313
kns3-3 <toma66kfc@docomo.ne.jp>
2026-04-09 17:10:03 +0900

    Lintエラー（コードスタイル）の修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller? #アクション前に独自メソッドを実行
+  before_action :configure_permitted_parameters, if: :devise_controller? # アクション前に独自メソッドを実行
 
-  protected #ApplicationControllerを継承した子クラス(Deviseなど)からのみアクセス可能
+  protected # ApplicationControllerを継承した子クラス(Deviseなど)からのみアクセス可能
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :image]) #sign_upの際にnameカラムの保存を許可
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :introduction, :image]) #account_updateの際にname,introductionカラムの保存を許可
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :name, :image ]) # sign_upの際にnameカラムの保存を許可
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :name, :introduction, :image ]) # account_updateの際にname,introductionカラムの保存を許可
   end
 end
--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,19 +1,19 @@
 class ReservationsController < ApplicationController
-  before_action :authenticate_user! #ログインしていないと予約できない
+  before_action :authenticate_user! # ログインしていないと予約できない
 
   def confirm
     @reservation = Reservation.new(reservation_params)
     @reservation.user_id = current_user.id
     @room = Room.find(params[:reservation][:room_id])
 
-    #バリデーションエラーがあれば詳細画面を読み込む
+    # バリデーションエラーがあれば詳細画面を読み込む
     if @reservation.invalid?
-      render "rooms/show"
+       render "rooms/show"
     else
-      #バリデーションが問題なければ計算処理をする
-      #終了日-開始日で宿泊日数を計算する
+      # バリデーションが問題なければ計算処理をする
+      # 終了日-開始日で宿泊日数を計算する
       @stay_days = (@reservation.end_date - @reservation.start_date).to_i
-      #施設料金×宿泊日数×人数で合計金額を計算する
+      # 施設料金×宿泊日数×人数で合計金額を計算する
       @total_price = @room.price * @stay_days * @reservation.person_count
     end
   end
@@ -25,8 +25,8 @@ class ReservationsController < ApplicationController
       if @reservation.save
         redirect_to reservations_path, notice: "予約が完了しました！"
       else
-        #保存失敗時、画面表示に必要なデータを揃え直す
-        #予約情報のルームIDからRoomテーブル内の情報を変数に入れる
+        # 保存失敗時、画面表示に必要なデータを揃え直す
+        # 予約情報のルームIDからRoomテーブル内の情報を変数に入れる
         @room =Room.find(@reservation.room_id)
         @stay_days = (@reservation.end_date - @reservation.start_date).to_i
         @total_price = @room.price * @stay_days * @reservation.person_count
@@ -35,7 +35,7 @@ class ReservationsController < ApplicationController
     end
 
     def index
-      #ログインしているユーザーの予約を予約日時の降順に並べて取得する
+      # ログインしているユーザーの予約を予約日時の降順に並べて取得する
       @reservations = current_user.reservations.order(created_at: :desc)
     end
 
@@ -47,7 +47,7 @@ class ReservationsController < ApplicationController
 
   private
 
-  #ストロングパラメータ
+  # ストロングパラメータ
   def reservation_params
     params.require(:reservation).permit(:start_date, :end_date, :person_count, :room_id, :user_id)
   end
